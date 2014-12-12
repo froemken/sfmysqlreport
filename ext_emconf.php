454390865f65f7c435ce576acaa275b1e15495da
Stefan Froemken <froemken@gmail.com>
2014-12-12 18:03:15 +0100

    add dependencies to TYPO3 6.2 and 7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,18 +32,15 @@ $EM_CONF[$_EXTKEY] = array (
 	'version' => '0.1.2',
 	'constraints' => 
 	array (
-		'depends' => 
-		array (
+		'depends' => array(
+			'typo3' => '6.2.0-7.1.99'
 		),
-		'conflicts' => 
-		array (
+		'conflicts' => array (
 		),
-		'suggests' => 
-		array (
+		'suggests' => array (
 		),
 	),
 	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"d3ad";s:33:"class.tx_sfmysqlreport_report.php";s:4:"8178";s:16:"ext_autoload.php";s:4:"47d9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"8ff7";s:13:"locallang.xml";s:4:"4da2";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"5747";s:20:"doc/wizard_form.html";s:4:"9abf";s:63:"reports/class.tx_sfmysqlreport_reports_innodbbufferpoolsize.php";s:4:"aa5b";s:52:"reports/class.tx_sfmysqlreport_reports_keybuffer.php";s:4:"c62f";s:63:"reports/class.tx_sfmysqlreport_reports_maxlengthforsortdata.php";s:4:"5de6";s:57:"reports/class.tx_sfmysqlreport_reports_readbuffersize.php";s:4:"8c3c";s:60:"reports/class.tx_sfmysqlreport_reports_readrndbuffersize.php";s:4:"dc6b";s:57:"reports/class.tx_sfmysqlreport_reports_sortbuffersize.php";s:4:"9622";s:53:"reports/class.tx_sfmysqlreport_reports_tablecache.php";s:4:"053f";s:35:"reports/tx_sfmysqlreport_report.gif";s:4:"1bdc";s:33:"reports/status/class.test.php.php";s:4:"3088";s:16:"res/css/main.css";s:4:"07fb";s:18:"res/css/screen.css";s:4:"84c2";}',
-	'suggests' => 
-	array (
+	'suggests' => array (
 	),
 );
