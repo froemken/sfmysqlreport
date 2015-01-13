8a71a2a0fc7f5676df93775c139999407157014e
Stefan Froemken <froemken@gmail.com>
2015-01-13 16:31:27 +0100

    save extConf into local var in ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) && !is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-}
+$extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
 if (
-	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileFrontend'] && TYPO3_MODE==='FE') ||
-	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileBackend'] && TYPO3_MODE==='BE')
+	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileFrontend'] && TYPO3_MODE === 'FE') ||
+	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileBackend'] && TYPO3_MODE === 'BE')
 ) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['setDBinit'] .= LF . ' SET profiling = 1;';
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'][] = 'StefanFroemken\\Sfmysqlreport\\Database\\DatabaseHooks';
