9bdba2a1f623cba9dcaf621df83d0711e2a71ca7
Stefan Froemken <froemken@gmail.com>
2015-01-13 16:38:57 +0100

    Forget to use the local var in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,8 @@ defined('TYPO3_MODE') or die();
 $extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
 if (
-	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileFrontend'] && TYPO3_MODE === 'FE') ||
-	($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['profileBackend'] && TYPO3_MODE === 'BE')
+	($extConf['profileFrontend'] && TYPO3_MODE === 'FE') ||
+	($extConf['profileBackend'] && TYPO3_MODE === 'BE')
 ) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['setDBinit'] .= LF . ' SET profiling = 1;';
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']['queryProcessors'][] = 'StefanFroemken\\Sfmysqlreport\\Database\\DatabaseHooks';
